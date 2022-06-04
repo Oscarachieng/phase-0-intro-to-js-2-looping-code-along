5a80d9268f7a1dcaedc9ce1c3a369a67199a59b8
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-04 22:56:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+//const gifts = ["teddy bear", "drone","doll"];
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event ="surprise";
+
+
+function writeCards(name,event){
+    const messages =[]
+    for (let index = 0; index < names.length; index++) {
+       // const element = names[index];
+        messages.push("Thank you, " +names[index]+ ", for the wonderful " +event+ " gift!")
+    }
+    return messages;
+}
+console.log(writeCards(names,"surprise"));
+
+function countDown() {
+    const myNumber = 10;
+    for (let i = myNumber; i >= 0; i--) {
+        //const element = array[index];
+        //ind.push(num);
+        console.log(i);
+        
+    }
+    //console.log(index);
+}
+countDown(10);
+//console.log(ind);
+   
+  
