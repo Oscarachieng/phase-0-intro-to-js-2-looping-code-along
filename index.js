8dad1439eb1623b29b405934229e175960844a77
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-10 13:12:12 +0300

    commit tree ba14d8728b6487c14c3a49160a54168b4c0770d7

--- index.js
+++ index.js
@@ -2,30 +2,24 @@
 //const gifts = ["teddy bear", "drone","doll"];
 
 const names = ["Guadalupe", "Ollie", "Aki"];
-const event ="surprise";
+const event = "surprise";
 
-
-function writeCards(name,event){
-    const messages =[]
-    for (let index = 0; index < names.length; index++) {
-       // const element = names[index];
-        messages.push("Thank you, " +names[index]+ ", for the wonderful " +event+ " gift!")
-    }
-    return messages;
+function writeCards(name, event) {
+  const messages = [];
+  for (let index = 0; index < names.length; index++) {
+    messages.push(
+      "Thank you, " + names[index] + ", for the wonderful " + event + " gift!"
+    );
+  }
+  return messages;
 }
-console.log(writeCards(names,"surprise"));
+console.log(writeCards(names, "surprise"));
 
 function countDown() {
-    const myNumber = 10;
-    for (let i = myNumber; i >= 0; i--) {
-        //const element = array[index];
-        //ind.push(num);
-        console.log(i);
-        
-    }
-    //console.log(index);
+  const myNumber = 10;
+  for (let i = myNumber; i >= 0; i--) {
+    console.log(i);
+  }
 }
 countDown(10);
 //console.log(ind);
-   
-  
